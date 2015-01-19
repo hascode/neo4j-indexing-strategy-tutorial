0d50d34943e38aeaacf9190c71e9910b7de056cc
Micha Kops <micha.kops@gmail.com>
2015-01-19 21:15:42 +0100

    Syntax for manual indexing example optimized.

--- src/main/java/com/hascode/tutorial/examples/ManualIndexingExample.java
+++ src/main/java/com/hascode/tutorial/examples/ManualIndexingExample.java
@@ -21,21 +21,37 @@ public class ManualIndexingExample {
 		try (Transaction tx = db.beginTx()) {
 			IndexManager indexManager = db.index();
 
-			Node book = db.createNode(CustomLabels.BOOK);
-			book.setProperty("title", "Some book");
-			Index<Node> bookIndex = indexManager.forNodes("books");
-			bookIndex.add(book, "title", "Some book");
+			Node book1 = db.createNode(CustomLabels.BOOK);
+			book1.setProperty("title", "Some book");
+			Node book2 = db.createNode(CustomLabels.BOOK);
+			book2.setProperty("title", "Another book");
 
-			Node author = db.createNode(CustomLabels.AUTHOR);
-			author.setProperty("name", "Al Bundy");
-			author.createRelationshipTo(book, CustomRelations.HAS_WRITTEN);
+			Index<Node> bookIndex = indexManager.forNodes("books");
+			bookIndex.add(book1, "title", "Some book");
+			bookIndex.add(book2, "title", "Another book");
+
+			Node author1 = db.createNode(CustomLabels.AUTHOR);
+			author1.setProperty("name", "Al Bundy");
+			Node author2 = db.createNode(CustomLabels.AUTHOR);
+			author2.setProperty("name", "Peggy Bundy");
+
 			Index<Node> authorIndex = indexManager.forNodes("authors");
-			authorIndex.add(author, "name", "Al Bundy");
+			authorIndex.add(author1, "name", "Al Bundy");
+			authorIndex.add(author2, "name", "Peggy Bundy");
+
+			author1.createRelationshipTo(book1, CustomRelations.HAS_WRITTEN);
+			author2.createRelationshipTo(book2, CustomRelations.HAS_WRITTEN);
 
 			ExecutionEngine engine = new ExecutionEngine(db);
-			String cql = "START author=node:authors(name = 'Al Bundy') MATCH (author:AUTHOR)-[:HAS_WRITTEN]->(book:BOOK) RETURN author, book";
-			ExecutionResult result = engine.execute(cql);
-			System.out.println(result.dumpToString());
+			// query for books written by Al Bundy
+			String cql1 = "MATCH (author:AUTHOR{name : 'Al Bundy'})-[:HAS_WRITTEN]->(book:BOOK) RETURN author, book";
+			ExecutionResult result1 = engine.execute(cql1);
+			System.out.println(result1.dumpToString());
+
+			// query for books written by Peggy Bundy
+			String cql2 = "MATCH (author:AUTHOR{name : 'Peggy Bundy'})-[:HAS_WRITTEN]->(book:BOOK) RETURN author, book";
+			ExecutionResult result2 = engine.execute(cql2);
+			System.out.println(result2.dumpToString());
 			tx.success();
 		}
 
